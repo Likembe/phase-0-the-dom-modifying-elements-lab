c5f592863cfd4e5467fa2c47f6b7d0d18854bdf1
Alpha <alpha.likembe@student.moringaschool.com>
2023-08-02 23:06:24 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if(mainElement) {
+    mainElement.remove();
+}
+
+const newHeader = document.getElementById('victory');
+if (newHeader) {
+    newHeader.textContent = "YOUR-NAME is the champion"
+}
